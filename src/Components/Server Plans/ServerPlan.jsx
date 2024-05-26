475e65984a87cd160dca5dd03d86a7848c7f688e
SilviuPe <silviupetrea60@gmail.com>
2024-05-26 14:29:27 -0700

    New folder for selection bar (new component)

--- src/Components/Server Plans/ServerPlan.jsx
+++ src/Components/Server Plans/ServerPlan.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
 //import './ServerPlan.css';
-import  { useState, useEffect } from 'react';
-import { scroll_page_down, scroll_page_up } from './ServerPlanFunctions.js';
-const SelectionBar = ({title}) => {
-    return (
-        <div className='my-4 flex items-center'>
-            <p className='flex-2 w-pc2'>{title}</p>
-            <div className='flex-1'>
-                <div className='SelectionBar bg-white_transparent flex w-full h-7  rounded-full shadow-md cursor-pointer pl-2 items-center'>
-                    <div 
-                        className='bg-cyan-500 text-white p-2 rounded-full h-full'
-                        style={{ borderRadius: '50%', width: '10px', height: '10px'}}> </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+import  { useState, useEffect, useRef } from 'react';
+import { scroll_page_down, scroll_page_up, calculate_horizontal_position } from './ServerPlanFunctions.js';
+import { SelectionBar, SelecionBar_Storage } from "../SelectionBar_CustomBuild/SelectionBARS.jsx";
+
+
+
 const Dropdown = () => {
         const [isOpen,setIsOpen] = useState(false);
+
 
         useEffect(() => {
             if (isOpen)
@@ -31,14 +22,14 @@ const Dropdown = () => {
             setIsOpen(!isOpen);
         }
         return (
-            <div className='relative'> 
+            <div className="relative">
                             <div className={`Dropdown mt-8 text-white rounded-md shadow-md transition-all duration-700 ease-out ${ isOpen ? 'opacity-100 h-full' : 'opacity-0 h-0'}`}>
                                 <h2 className='dropdown_custom_title font-bold text-center text-2xl'>Customize your configuration</h2>
                                 <div className='configuration_main_container flex mt-m mx-x3l transition-all duration-700' style={{ maxHeight: isOpen ? '100%' : '0', overflow : 'hidden'}}>
                                     <div className=' configuration_options justify-center w-full items-center flex-2 mr-x3l'>
-                                        <SelectionBar title = "CPUs: "/>
-                                        <SelectionBar title = "RAM Memory:"/>
-                                        <div className='my-4'>Storage:</div>
+                                        <SelectionBar title = "CPUs: " type = 'cpu'/>
+                                        <SelectionBar title = "RAM Memory:" type = 'ram'/>
+                                        <SelecionBar_Storage/>
                                         <SelectionBar title = "Network Speed: "/>
                                         <div className='my-4'>Operating System:</div>
                                         <div className='my-4'>IP Address Type:</div>
@@ -63,13 +54,43 @@ const Dropdown = () => {
 }
 
 const ServerPlan = () => {
+    const [opacity,setOpacity] = useState(0);
+    const [plansOpacity, setPlansOpacity] = useState([0,0,0]);
+
+    useEffect(() => {
+        setOpacity(1);
+    },[])
+
+    useEffect(() => {
+        const timer_silver = setTimeout(() => {
+            setPlansOpacity([100,0,0])
+        }, 1000)
+        return () => clearTimeout(timer_silver);
+    },[])
+
+    useEffect(() => {
+        const timer_silver = setTimeout(() => {
+            setPlansOpacity([100,100,0])
+        }, 1500)
+        return () => clearTimeout(timer_silver);
+    },[])
+
+    useEffect(() => {
+        const timer_gold = setTimeout(() => {
+            setPlansOpacity([100,100,100])
+        }, 1900)
+        return () => clearTimeout(timer_gold);
+    },[])
+
   return (
-    <div className='ServerPlan_main_container font-extralight'>
+    <div className={`ServerPlan_main_container font-extralight`}> 
         <div className='ServerPlan_title_container items-center justify-center mt-xl mb-xl'>
-            <h2 className='text-center font-bold text-2xl text-white'>Server Plans</h2>
+            <h2 className={`text-center font-bold text-2xl text-white  opacity-${opacity}`} style={{transition: 'opacity 2s'}}>Server Plans</h2>
         </div>
-        <div className='ServerPlans flex space-x-xl items-center justify-center'>
-            <div className='bronze_plan text-bronze_color border-2 border-transparent hover:border-bronze_color transition duration-300 ease-in-out p-s rounded-lg'>
+        <div className="ServerPlans flex space-x-xl items-center justify-center">
+            <div className={`bronze_plan text-bronze_color border-2 border-transparent hover:border-bronze_color 
+            transition duration-300 ease-in-out p-s rounded-lg
+            opacity-${plansOpacity[0]}`} style = {{transition: 'opacity 1.5s'}}>
                 <div className='title_plan text-center mb-s'> Bronze Plan</div>
                 <div className='configuration space-y-6 px-6 rounded-md border-y border-bronze_color pb-8 pt-s'>
                     <div className='cpu'>vCPU: x1</div>
@@ -81,7 +102,9 @@ const ServerPlan = () => {
                 <div className='pl-s pt-6 text-center mt-6'> <p>Price: <b className='font-bold text-2'>4.99$ / month</b></p></div>
             </div>
             <div className='line_separator'></div>
-            <div className='silver_plan text-silver_color border-2 border-transparent hover:border-silver_color transition duration-300 ease-in-out p-s rounded-lg'>
+            <div className={`silver_plan text-silver_color border-2 border-transparent hover:border-silver_color 
+            transition duration-300 ease-in-out p-s rounded-lg
+            opacity-${plansOpacity[1]}`} style = {{transition: 'opacity 1.5s'}}>
                 <div className='title_plan text-center mb-s'> Silver Plan</div>
                 <div className='configuration space-y-6 px-6 rounded-md border-y border-silver_color pb-8 pt-s '>
                     <div className='cpu'>vCPU: x2</div>
@@ -93,7 +116,9 @@ const ServerPlan = () => {
                 <div className='pl-s pt-6 text-center mt-6'> <p>Price: <b className='font-bold'>49.99$ / month</b></p></div>
             </div>
             <div className='line_separator'></div>
-            <div className='gold_plan text-gold_color border-2 border-transparent hover:border-gold_color transition duration-300 ease-in-out p-s rounded-lg'>
+            <div className={`gold_plan text-gold_color border-2 border-transparent hover:border-gold_color 
+            transition duration-300 ease-in-out p-s rounded-lg
+            opacity-${plansOpacity[2]}`} style = {{transition: 'opacity 1.5s'}}>
                 <div className='title_plan text-center mb-s'> Gold Plan</div>
                 <div className='configuration space-y-6 px-6 rounded-md border-y border-gold_color pb-8 pt-s'>
                     <div className='cpu'>vCPU: x4</div>
--- src/Components/Server Plans/ServerPlanFunctions.js
+++ src/Components/Server Plans/ServerPlanFunctions.js
@@ -1,3 +1,5 @@
+import { act } from "react";
+
 export const scroll_page_up = () => {
     window.scrollTo({
         top: 0,
@@ -12,4 +14,29 @@ export const scroll_page_down = () => {
         top: document.body.scrollHeight,
         behavior: 'smooth'
       });
+}
+
+
+
+export const calculate_horizontal_position = (difference, actual_position, type) => {
+  let minimum =  difference
+  
+  let coords = 0;
+  let count = 1;
+  
+  for( let i = 0; i <= 7; i++) {
+      let pozitive_number = Math.abs(actual_position - (i * difference));
+      
+      if (pozitive_number < minimum ) {
+          minimum = actual_position - (i * difference);
+          coords = i * difference;
+          count = i+1;
+      }
+  }
+  console.log(difference,coords)
+  console.log("Actual pos:", actual_position)
+  return {
+    coords : coords,
+    count : count
+  }
 }
