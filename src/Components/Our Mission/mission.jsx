9d351e7769ae12b0bb2583e1b53d90992e5b19bc
SilviuPe <silviupetrea60@gmail.com>
2024-06-02 23:47:02 -0700

    Added mission section

--- src/Components/Our Mission/mission.jsx
+++ src/Components/Our Mission/mission.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 
 function Mission() {
   return (
-    <></>
+      <div className='Mission_content px-s'>
+        <div className='title'>
+          <h2 className=''>That We Offer</h2>
+        </div>
+        <div className='services flex justify-between'>
+          <div className='custom_build'>
+            Custom Build
+          </div>
+          <div className='support'>
+            Support 24/7
+          </div>
+          <div className='unlimited traffic'>
+            Unlimited Traffic
+          </div>
+        </div>
+      </div>
   );
 }
 
