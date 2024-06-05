97e927597ab541af4d610fa64915e8ee33fa3b98
SilviuPe <silviupetrea60@gmail.com>
2024-06-04 21:24:35 -0700

    Added new function for scrolling to the plans

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,6 +5,11 @@ const Header_Functions = (id) => {
         html_element.scrollIntoView({behavior : 'smooth'})
     }
 
+    const scroll_to_specific_component = (component_id) => {
+        const html_element = document.getElementById(component_id);
+        html_element.scrollIntoView({behavior : 'smooth'})
+    }
+
     return {
         scroll_to_this_component,
     }
