1b2344c674d952b5745f75041e153967e3866bf7
SilviuPe <silviupetrea60@gmail.com>
2024-06-12 09:42:22 -0700

    Exporting the new components

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export {default as ServerPlan} from './Server Plans/ServerPlan.jsx';
 export {default as Offer} from './Offers/Offers.jsx';
 export {default as Footer} from './Footer/footer.jsx';
 export {default as OurMission} from './Mission/mission.jsx';
+export {default as Question} from './Question/Question.jsx';
--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,3 +1,4 @@
 export {default as HomePage} from './Home Page/HomePage.jsx';
 export {default as Register} from './Register Page/Register.jsx';
 export {default as Login} from './Login Page/Login.jsx';
+export {default as FAQ} from './FAQ/FAQ.jsx';
