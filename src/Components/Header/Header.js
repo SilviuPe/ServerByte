ba38c531670b7a7a0ced5d1bcff60164e5eb9f03
SilviuPe <silviupetrea60@gmail.com>
2024-06-12 09:39:54 -0700

    Added a new function to navigate to the FAQ page.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,9 +10,15 @@ const Header_Functions = (id) => {
         html_element.scrollIntoView({behavior : 'smooth'})
     }
 
+
+    const go_to_FAQ_page = () => {
+        window.location.href = '/faq_page';
+    }
+
     return {
         scroll_to_this_component,
-        scroll_to_specific_component
+        scroll_to_specific_component,
+        go_to_FAQ_page
     }
 }
 
