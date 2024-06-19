7b105fa57a6ba69e49aa962d210104b0f1d2d236
SilviuPe <silviupetrea60@gmail.com>
2024-06-19 09:41:11 -0700

    Update

--- src/Components/Mission/mission.jsx
+++ src/Components/Mission/mission.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Lottie from 'lottie-react';
 import animationData from '../../Assets/Animation.json';
 
 
 const OurMission = () => {
-  return (
-    <div className='OurMission bg-contect_bg_color text-white pt-l px-x3l flex pb-m'>
-        <div className='flex-1 flex items-center justify-center'>
+
+    const [opacity,setOpacity] = useState([0,0]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setOpacity([100,100])
+        }, 1000)
+        return () => clearTimeout(timer);
+    }, [])
+
+    return (
+    <div className={`OurMission relative bg-contect_bg_color text-white pt-l px-x3l flex pb-m`}>
+        <div className={`flex-1 flex items-center justify-center transition-all duration-1000 ease-in-out opacity-${opacity[0]} ${opacity[0] ? 'left-0' : 'left-[-50px]'}`}>
             <div>
                 <div className='title'>
                     <h1 className='text-8xl font-bold'>
@@ -19,11 +29,11 @@ const OurMission = () => {
                 </div>
             </div>
         </div>
-        <div className='animation flex-1 items-center justify-center'>
+        <div className={`animation flex-1 items-center justify-center transition-all duration-1000 ease-in-out opacity-${opacity[1]} ${opacity[1] ? 'top-0' : 'top-[-50px]'}`}>
             <Lottie animationData={animationData} loop={true}/>
         </div>
     </div>
-  )
+    )
 }
 
 export default OurMission
