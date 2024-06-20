918ab310d4565ee5f12bcc742fad9fad0fe3dfaf
SilviuPe <silviupetrea60@gmail.com>
2024-06-20 03:36:57 +0300

    Better written code.

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -51,22 +51,15 @@ function Mission() {
       <div className='title mb-l'>
         <h2 className='font-semibold text-5xl text-center'>Explore Our Services</h2>
       </div>
-      <InfoField description_side = 'r' animation = {supportAnimationData} description = "Server Byte offers exceptional customer support, available around the clock. 
-              Our team is ready to assist you 24/7 with configuration choices, server management, 
-              and billing inquiries. You can reach us via phone, email, or our live chat platform."/>
-    <InfoField description_side = 'l' animation = {mobileAnimationData} description = "With ServerByte, you can securely connect to your servers using SSH directly from your mobile device. 
-            This feature ensures that you have the flexibility to perform critical 
-            tasks and manage your infrastructure on the go."/>
+      <InfoField description_side = 'r' animation = {supportAnimationData} description = "Server Byte offers exceptional customer support, available around the clock. Our team is ready to assist you 24/7 with configuration choices, server management, and billing inquiries. You can reach us via phone, email, or our live chat platform."/>
+      
+      <InfoField description_side = 'l' animation = {mobileAnimationData} description = "With ServerByte, you can securely connect to your servers using SSH directly from your mobile device. This feature ensures that you have the flexibility to perform critical tasks and manage your infrastructure on the go."/>
+      
+      <InfoField description_side = 'r' animation = {customAnimationData} description = "Server Byte provides you with the opportunity to select a custom build at an affordable price. With just a few clicks, you can configure your environment to meet your specific needs."/>
 
-    <InfoField description_side = 'r' animation = {customAnimationData} description = "Server Byte provides you with the opportunity to select a custom build at an affordable price. 
-              With just a few clicks, you can configure your environment to meet your specific needs."/>
-
-    <InfoField description_side = 'l' animation = {affordableAnimationData} description = "At ServerByte, we believe that cutting-edge technology should be accessible to everyone. 
-            Our pricing model is designed to offer exceptional value, 
-            making high-quality server management tools affordable without compromising on performance or security. "/>
+      <InfoField description_side = 'l' animation = {affordableAnimationData} description = "At ServerByte, we believe that cutting-edge technology should be accessible to everyone. Our pricing model is designed to offer exceptional value, making high-quality server management tools affordable without compromising on performance or security. "/>
     
-    <InfoField description_side = 'r' animation = {trafficAnimationData} description = "Our servers come with unlimited traffic capabilities, allowing you to transfer data without any restrictions. 
-              This ensures you can handle as much data as necessary for your operations."/>
+      <InfoField description_side = 'r' animation = {trafficAnimationData} description = "Our servers come with unlimited traffic capabilities, allowing you to transfer data without any restrictions. This ensures you can handle as much data as necessary for your operations."/>
     </div>
     );
 }
