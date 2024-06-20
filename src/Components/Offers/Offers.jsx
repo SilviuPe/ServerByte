aedf807af3c23e66cc57901ec010fa034cea5f91
SilviuPe <silviupetrea60@gmail.com>
2024-06-20 03:32:18 +0300

    Implementing new style on our services section from home page

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -5,7 +5,35 @@ import SupportIMG from '../../Assets/SupportTeam.png';
 import MobileIMG from '../../Assets/MobileSSH.png';
 import UnlimitedTrafficIMG from '../../Assets/UnlimitedNetwork.png';
 import BestPricesIMG from '../../Assets/UnbeatablePrices.png';
+import Lottie from 'lottie-react';
 
+
+import supportAnimationData from '../../Assets/supportAnimation.json';
+import mobileAnimationData from '../../Assets/mobileAnimation.json';
+import customAnimationData from '../../Assets/customAnimation.json';
+import affordableAnimationData from '../../Assets/affordableAnimation.json';
+import trafficAnimationData from '../../Assets/trafficAnimation.json';
+
+const InfoField = ({description_side, animation, description}) => {
+
+    const components = [
+      <div className={`Animation flex-1 w-full flex justify-center ${ description_side === 'r' ? 'pr-x2l' : 'pl-x2l'}`}>
+        <Lottie style = {{width: 300}} animationData={animation} loop={true}/>
+      </div>,
+
+      <div className={`Description flex-1 w-full flex justify-center text-2xl ${ description_side === 'r' ? 'pl-x2l' : 'pr-x2l'}`}>
+        <p className='px-l text-center'>{description}</p>
+      </div>
+    ];
+
+    return (
+      <>
+          <div className='InfoField flex items-center justify-center mt-x2l'>
+              { description_side === 'l' ? components : components.reverse()}
+          </div>
+      </>
+    )
+}
 
 function Mission() {
 
@@ -14,78 +42,31 @@ function Mission() {
     useEffect(() => {
       const timer = setTimeout(() => {
         setOpacity(100);
-      }, 1500 )
+      }, 1000 )
       return () => clearTimeout(timer);
     },[])
 
     return (
-    <div className={`Mission_content relative px-m text-white pt-xl bg-contect_bg_color transition-all duration-1000 ease-in-out transform opacity-${opacity} ${opacity ? 'bottom-0' : 'bottom-[-200px]'}`}>
+    <div className={`Mission_content relative px-m mt-l text-white pt-xl bg-contect_bg_color transition-all duration-1000 ease-in-out transform opacity-${opacity} ${opacity ? 'bottom-0' : 'bottom-[-200px]'}`}>
       <div className='title mb-l'>
-        <h2 className='font-semibold text-4xl text-center'>Explore Our Services</h2>
+        <h2 className='font-semibold text-5xl text-center'>Explore Our Services</h2>
       </div>
-      <div className='services flex px-x2l justify-between'>
-        <div className='custom_build w-pc3 px-x bg-content_bg_darker_color shadow-2xl-colored-fade py-x rounded-xl mx-s transition-all transition-700 hover:shadow-2xl-colored-bright'>
-          <div className='flex'>
-              <img style = {{width:'20px', height:'20px'}} src = {CustomIMG}/>
-              <h3 style = {{fontWeight: 500, paddingLeft: '5%'}}><span className = "cursor-pointer no-underline hover:underline">Custom Build</span></h3>
-            </div>
-          <div className='description mt-x'>
-            <p className='text-lg'>Server Byte provides you with the opportunity to select a custom build at an affordable price. 
-              With just a few clicks, you can configure your environment to meet your specific needs.
-            </p>
-          </div>
-        </div>
-        <div className='support w-pc3 px-x bg-content_bg_darker_color shadow-2xl-colored-fade py-x rounded-xl mx-s transition-all transition-700 hover:shadow-2xl-colored-bright'>
-          <div className='flex'>
-            <img style = {{width:'20px', height:'20px'}} src = {SupportIMG}/>
-            <h3 style = {{fontWeight: 500, paddingLeft: '5%'}}><span className = "cursor-pointer no-underline hover:underline">Support 24/7</span></h3>
-          </div>
-          <div className='description mt-x'>
-            <p className='text-lg'> Server Byte offers exceptional customer support, available around the clock. 
+      <InfoField description_side = 'r' animation = {supportAnimationData} description = "Server Byte offers exceptional customer support, available around the clock. 
               Our team is ready to assist you 24/7 with configuration choices, server management, 
-              and billing inquiries. You can reach us via phone, email, or our live chat platform.
-            </p>
-          
-          </div>
-        </div>
-        <div className='unlimited traffic w-pc3 px-x bg-content_bg_darker_color shadow-2xl-colored-fade py-x rounded-xl mx-s transition-all transition-700 hover:shadow-2xl-colored-bright'>
-          <div className='flex'>
-              <img style = {{width:'20px', height:'20px'}} src = {UnlimitedTrafficIMG}/>
-              <h3 style = {{fontWeight: 500, paddingLeft: '5%'}}><span className = "cursor-pointer no-underline hover:underline">Unlimited Traffic</span></h3>
-          </div>
-          <div className='description mt-x'>
-            <p className='text-lg'>Our servers come with unlimited traffic capabilities, allowing you to transfer data without any restrictions. 
-              This ensures you can handle as much data as necessary for your operations.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className='services_row_2 flex px-x3l justify-center mt-m'>
-        <div className='mobile_ssh w-pc3 px-x bg-content_bg_darker_color shadow-2xl-colored-fade py-x rounded-xl mx-s transition-all transition-700 hover:shadow-2xl-colored-bright'>
-          <div className='flex'>
-            <img style = {{width:'20px', height:'20px'}} src = {MobileIMG}/>
-            <h3 style = {{fontWeight: 500, paddingLeft: '5%'}}><span className = "cursor-pointer no-underline hover:underline">Mobile SSH</span></h3>
-          </div>
-          <div className='description mt-x'>
-            <p className='text-lg'>With ServerByte, you can securely connect to your servers using SSH directly from your mobile device. 
+              and billing inquiries. You can reach us via phone, email, or our live chat platform."/>
+    <InfoField description_side = 'l' animation = {mobileAnimationData} description = "With ServerByte, you can securely connect to your servers using SSH directly from your mobile device. 
             This feature ensures that you have the flexibility to perform critical 
-            tasks and manage your infrastructure on the go.
-            </p>
-          </div>
-        </div>
-        <div className='affordability w-pc3 px-x bg-content_bg_darker_color shadow-2xl-colored-fade py-x rounded-xl mx-xl transition-all transition-700 hover:shadow-2xl-colored-bright'>
-          <div className='flex'>
-            <img style = {{width:'20px', height:'20px'}} src = {BestPricesIMG}/>
-            <h3 style = {{fontWeight: 500, paddingLeft: '5%'}}><span className = "cursor-pointer no-underline hover:underline">Unbeatable Affordability</span></h3>
-          </div>
-          <div className='description mt-x'>
-            <p className='text-lg'> At ServerByte, we believe that cutting-edge technology should be accessible to everyone. 
+            tasks and manage your infrastructure on the go."/>
+
+    <InfoField description_side = 'r' animation = {customAnimationData} description = "Server Byte provides you with the opportunity to select a custom build at an affordable price. 
+              With just a few clicks, you can configure your environment to meet your specific needs."/>
+
+    <InfoField description_side = 'l' animation = {affordableAnimationData} description = "At ServerByte, we believe that cutting-edge technology should be accessible to everyone. 
             Our pricing model is designed to offer exceptional value, 
-            making high-quality server management tools affordable without compromising on performance or security. 
-            </p>
-          </div>
-        </div>
-      </div>
+            making high-quality server management tools affordable without compromising on performance or security. "/>
+    
+    <InfoField description_side = 'r' animation = {trafficAnimationData} description = "Our servers come with unlimited traffic capabilities, allowing you to transfer data without any restrictions. 
+              This ensures you can handle as much data as necessary for your operations."/>
     </div>
     );
 }
